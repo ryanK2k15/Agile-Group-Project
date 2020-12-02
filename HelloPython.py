cc896eae9a3b40e84750505a9acb4fd6860227a9
Ryan Kelleher <ryan.kelleher1@mycit.ie>
2020-12-02 11:26:31 +0000

    Change made

--- HelloPython.py
+++ HelloPython.py
@@ -3,3 +3,6 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
+
+numChanges = 0;
+print("Changes made: ", numChanges+1)
